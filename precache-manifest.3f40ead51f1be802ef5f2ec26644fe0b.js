798abac25fc8760338256fd350c99e2abe293ca5
Akshay Dhanji <akshay.dhanji@powerweave.com>
2019-12-23 16:54:46 +0530

    Updates

---
+++ precache-manifest.3f40ead51f1be802ef5f2ec26644fe0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f321040920c9575959782daff9f3b4d1",
+    "url": "/ReactRouterApp/index.html"
+  },
+  {
+    "revision": "097d7c08d83eef4b9672",
+    "url": "/ReactRouterApp/static/css/main.d8e89e44.chunk.css"
+  },
+  {
+    "revision": "b39cc7a1d7694d3bcc7e",
+    "url": "/ReactRouterApp/static/js/2.dc1e9083.chunk.js"
+  },
+  {
+    "revision": "097d7c08d83eef4b9672",
+    "url": "/ReactRouterApp/static/js/main.41a039e3.chunk.js"
+  },
+  {
+    "revision": "316cdf83815100963618",
+    "url": "/ReactRouterApp/static/js/runtime-main.a889b0ff.js"
+  }
+]);
